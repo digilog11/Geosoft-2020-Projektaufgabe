65030e774764750a1cfebbe77cf2570694011e68
digilog11 <66838684+digilog11@users.noreply.github.com>
2020-08-18 01:04:00 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,130 @@
+"use_strict";
+
+const express = require("express");
+const mongodb = require("mongodb");
+const port = 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+/**
+ * function which creates a Connection to MongoDB and the collections users, doctors and rides.
+ * Retries every 3 seconds if no connection could be established.
+ */
+async function connectMongoDB() {
+  try{
+    // connect to database server
+    app.locals.dbConnection = await mongodb.MongoClient.connect("mongodb://localhost:27017",
+    {useUnifiedTopology: true, useNewUrlParser: true});
+    // connect do database "geosoft"
+    app.locals.db = await app.locals.dbConnection.db("geosoft");
+    console.log("Using db: " + app.locals.db.databaseName);
+    // create collection
+    try{
+      app.locals.db.createCollection("users");
+      console.log("Collection users created");
+      app.locals.db.createCollection("doctors");
+      console.log("Collection doctors created");
+      app.locals.db.createCollection("rides");
+      console.log("Collection rides created");
+    }
+    catch (error){
+      console.dir(error);
+      setTimeout(connectMongoDB, 3000);
+    }
+  }
+  catch (error){
+    console.dir(error);
+    setTimeout(connectMongoDB, 3000);
+  }
+}
+
+//Start connecting
+connectMongoDB();
+
+/**
+ * inserts a document into the MongoDB collection users
+ * @param {object} user - user {username, password}
+ */
+ function insertUserMongo (user) {
+    app.locals.db.collection("users").insertOne(user, (error, result) => {
+     if (error){
+        console.dir(error);
+      }
+      console.log("1 document inserted in collection users");
+    });
+ }
+
+ /**
+  * inserts a document into the MongoDB collection doctors
+  * @param {object} doctor - doctor {username, password}
+  */
+  function insertDoctorMongo (doctor) {
+     app.locals.db.collection("doctors").insertOne(doctor, (error, result) => {
+      if (error){
+         console.dir(error);
+       }
+       console.log("1 document inserted in collection doctors");
+     });
+  }
+
+// Make all Files stored in Folder "public" accessible over localhost:3000/public
+app.use("/public", express.static(__dirname + "/public"));
+
+// Send startpage.html on request to "/"
+app.get("/", (req,res) => {
+  res.sendFile(__dirname + "/startpage.html")
+});
+
+// Send user.html on request to "/user"
+app.get("/user", (req,res) => {
+  res.sendFile(__dirname + "/user.html")
+});
+
+// Send doctor.html on request to "/doctor"
+app.get("/doctor", (req,res) => {
+  res.sendFile(__dirname + "/doctor.html")
+});
+
+
+// takes request body from userCreated-Post-Request and gives it on to function
+// insertUserMongo()
+app.post("/userCreated", (req,res) => {
+  var user = req.body;
+  insertUserMongo(user);
+})
+
+// takes request body from doctorCreated-Post-Request and gives it on to function
+// insertDoctorMongo()
+app.post("/doctorCreated", (req,res) => {
+  var user = req.body;
+  insertDoctorMongo(user);
+})
+
+// Returns all documents stored in collection users
+app.get("/users", (req,res) => {
+  app.locals.db.collection("users").find({}).toArray((error, result) => {
+    if (error){
+      console.dir(error);
+    }
+    res.json(result);
+  });
+});
+
+// Returns all documents stored in collection doctors
+app.get("/doctors", (req,res) => {
+  app.locals.db.collection("doctors").find({}).toArray((error, result) => {
+    if (error){
+      console.dir(error);
+    }
+    res.json(result);
+  });
+});
+
+// listen on port 3000
+app.listen(port,
+  () => console.log("app listening at http://localhost:3000")
+);
